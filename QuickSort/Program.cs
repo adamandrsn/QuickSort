271b4450e907ad5af2a7035bafb30df04c4cd8f9
Adam Anderson <adam.anderson.ft21@mail.umy.ac.id>
2022-10-19 17:44:56 +0700

    menambahkan method q_sort dan lopping while

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -15,7 +15,7 @@ namespace QuickSort
 
         //number of elements in array
         private int n;
-
+        private int low;
 
         void input()
         {
@@ -34,22 +34,61 @@ namespace QuickSort
             Console.WriteLine("\n======================");
 
             //get array elements
-            for(int i = 0; i < arr.Length; i++)
+            for (int i = 0; i < arr.Length; i++)
             {
                 Console.Write("<" + (i + 1) + ">");
                 string s1 = Console.ReadLine();
                 arr[i] = Int32.Parse(s1);
             }
             //swaps the element at index x with the element at index y
+        }
+        void swap(int x,int y)
+        {
+            int temp;
+            temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = temp;
+        }
+            
+        public void q_sort(int low, int high)
+        {
+            int pivot, i, j;
+            if (low < high)
+                return;
 
-            void swap(int x,int y)
+            //Patition the list into two parts;
+            //One containing elements less that or equal to pivot
+            //Outer countaining element greather than pivot
+
+            i = low;
+            j = high;
+
+            pivot = arr[low];
+
+            while (i <= j)
             {
-                int temp;
-                temp = arr[x];
-                arr[x] = arr[y];
-                arr[y] = temp;
-            }
+                //Search fot an element greater than pivot
+                while ((arr[i] <= pivot) && (j <= high))
+                {
+                    i++;
+                    cmp_count++;
+                }
+                cmp_count++;
 
+                //Search for an elements less than or equal to pivot
+                while ((arr[i] > pivot) && (j >= low))
+                {
+                    j--;
+                    cmp_count++;
+                }
+                cmp_count--;
+
+                if (i < j)
+                {
+                    swap(i, j);
+                    mov_count++;
+                }
+            }
         }
     }
 }
