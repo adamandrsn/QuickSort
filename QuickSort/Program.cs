1009d41c17860c4146305d7b3399208ab3466c75
Adam Anderson <adam.anderson.ft21@mail.umy.ac.id>
2022-10-19 18:10:25 +0700

    add method main str

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -122,5 +122,21 @@ namespace QuickSort
         {
             return (n);
         }
+
+        static void Main(String[] args)
+        {
+            //Declaring the object of the class
+            program myList = new program();
+            //Accept Array elements
+            myList.input();
+            //Calling the sorting function
+            //First call to Quick sort Algorithm
+            myList.q_sort(0, myList.getSize() - 1);
+            //Display sorted array
+            myList.display();
+            // to exit from the console
+            Console.WriteLine("\n\nPress Enter to exit.");
+            Console.Read()
+        }
     }
 }
