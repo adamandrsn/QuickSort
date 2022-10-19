422339c0a77de21c98d07983f8cf378e20ff7ef2
Adam Anderson <adam.anderson.ft21@mail.umy.ac.id>
2022-10-19 17:16:04 +0700

    add method swap

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -40,6 +40,15 @@ namespace QuickSort
                 string s1 = Console.ReadLine();
                 arr[i] = Int32.Parse(s1);
             }
+            //swaps the element at index x with the element at index y
+
+            void swap(int x,int y)
+            {
+                int temp;
+                temp = arr[x];
+                arr[x] = arr[y];
+                arr[y] = temp;
+            }
 
         }
     }
