c86c24a57144ba1ef85c59da64addd8f2c3b01dc
Adam Anderson <adam.anderson.ft21@mail.umy.ac.id>
2022-10-19 16:55:05 +0700

    array of integers to hold values & number of element in array

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace QuickSort
 {
-    internal class Program
-    {
+    class program
+    { 
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; // number of comparasion
+        private int mov_count = 0; // number of data movements
+
+        //number of elements in array
+        private int n;
+    
+    
         static void Main(string[] args)
         {
         }
